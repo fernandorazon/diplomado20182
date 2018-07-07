302ad6da03df72013578280919a394d3d0fa4fef
Fernando Razon <fernandorazon@Jaimes-MBP.lan>
2018-07-06 21:28:32 -0500

    Tareas aun en proceso

---
+++ WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotter
+//
+//  Created by Fernando Razon on 7/6/18.
+//  Copyright © 2018 d182_fernando_r. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+        
+}
