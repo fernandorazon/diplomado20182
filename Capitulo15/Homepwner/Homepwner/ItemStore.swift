915a79bdada4b0bfe16425d5a3d32f0812b5b9da
Fernando Razon <fernandorazon@Jaimes-MBP.lan>
2018-07-15 22:20:10 -0500

    Chapter 11 finished in order to finish chapter 15

--- Capitulo15/Homepwner/Homepwner/ItemStore.swift
+++ Capitulo15/Homepwner/Homepwner/ItemStore.swift
@@ -12,12 +12,12 @@ import UIKit
 class ItemStore {
     var allItems = [Item]()
     
-    //Este constructor genera 5 items de inicio
-    init() {
-        for _ in 0..<5 {
-            createItem()
-        }
-    }
+//    //Este constructor genera 5 items de inicio
+//    init() {
+//        for _ in 0..<5 {
+//            createItem()
+//        }
+//    }
     
     //Este metodo creara un nuevo item random llamando a un init de la clase Item
     @discardableResult func createItem() -> Item {
@@ -25,7 +25,27 @@ class ItemStore {
         allItems.append(newItem)
         return newItem
     }
+    
+    //Este metodo borra celdas
+    func removeItem(_ item: Item) {
+        if let index = allItems.index(of: item) {
+            allItems.remove(at: index)
+        }
+    }
 
+    //Este metodo permite cambiar el lugar que ocupa cada item en el arreglo
+    func moveItem(from fromIndex: Int, to toIndex: Int) {
+        if fromIndex == toIndex {
+            return }
+        // Get reference to object being moved so you can reinsert it
+        let movedItem = allItems[fromIndex]
+        // Remove item from array
+        allItems.remove(at: fromIndex)
+        // Insert item in array at new location
+        allItems.insert(movedItem, at: toIndex)
+    }
+    
+    
     
 }
 
--- Capitulo15/Homepwner/Homepwner/ItemsViewController.swift
+++ Capitulo15/Homepwner/Homepwner/ItemsViewController.swift
@@ -13,6 +13,8 @@ class ItemsViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
+    
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -30,25 +32,88 @@ class ItemsViewController: UITableViewController {
         return itemStore.allItems.count
     }
     
+    //Formato a las celdas
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         // Instancia de la celda con estilo default
         //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
-        
         //Instancia o reusa una celda
         let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
-        
         // Set the text on the cell with the description of the item
         // that is at the nth index of items, where n = row this cell
         // will appear in on the tableview
         let item = itemStore.allItems[indexPath.row]
-        
         //Se agrega texto a las subvistas de la celda con el nombre de los items
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        
         return cell
     }
+    
+    //Borrar tanto en el itemStore como de la tabla
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            //Se agrega un AlertController antes de borrar una celda
+            let title = "Delete \(item.name)?"
+            let message = "Are you sure you want to delete this item?"
+            let ac = UIAlertController(title: title,message: message, preferredStyle: .actionSheet)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive,
+                                             handler: { (action) -> Void in
+                                                // Remove the item from the store
+                                                self.itemStore.removeItem(item)
+                                                // Also remove that row from the table view with an animation
+                                                self.tableView.deleteRows(at: [indexPath], with: .automatic) })
+            ac.addAction(deleteAction)
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
+        }
+    }
+    
+    //Mover las celdas tanto en la tabla como en el item store
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
+    //Estos metodos son llamados al tapear los botones correspondientes
+    
+    @IBAction func addNewItem(_ sender: UIButton) {
+        
+//        // Make a new index path for the 0th section, last row
+//        let lastRow = tableView.numberOfRows(inSection: 0)
+//        let indexPath = IndexPath(row: lastRow, section: 0)
+//        // Insert this new row into the table
+//        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        // Create a new item and add it to the store
+        let newItem = itemStore.createItem()
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            // Insert this new row into the table
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+        
+    }
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        
+        // If you are currently in editing mode...
+        if isEditing {
+            // Change text of button to inform user of state
+            sender.setTitle("Edit", for: .normal)
+            // Turn off editing mode
+            setEditing(false, animated: true)
+        } else {
+            // Change text of button to inform user of state
+            sender.setTitle("Done", for: .normal)
+            // Enter editing mode
+            setEditing(true, animated: true)
+        }
+        
+    }
+
     
 }
 
